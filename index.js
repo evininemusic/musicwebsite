bf62870a6362e25f11b5334241c69cdb0927b1d7
ev <156191277+evininemusic@users.noreply.github.com>
2025-11-19 22:22:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
 function openMenu() {
     // Makes menu and page cover visible.
     document.getElementById("js-page-cover").style.display = "block";
-    document.getElementById("js-menu").style.width = "26rem";
-    document.getElementById("js-menu-website-link-1").style.fontSize = "2.7rem";
-    document.getElementById("js-menu-website-link-2").style.fontSize = "2.7rem";
-    document.getElementById("js-menu-website-link-3").style.fontSize = "2.7rem";
-    document.getElementById("js-close-menu-button").style.width = "2rem";
+    document.getElementById("js-menu").style.width = "100%";
+    document.getElementById("js-menu-website-link-1").style.fontSize = "1.7rem";
+    document.getElementById("js-menu-website-link-2").style.fontSize = "1.7rem";
+    document.getElementById("js-menu-website-link-3").style.fontSize = "1.7rem";
+    document.getElementById("js-close-menu-button").style.width = "1.5rem";
     // Adds menu close function when escape is pressed while open.
     document.body.addEventListener('keydown', function(e) {
         if (e.key == "Escape") {
